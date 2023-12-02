09ae6c6e0cdd9de8d316cf8d896e34d74a0adef0
Jtan99 <jtantan99@gmail.com>
2023-12-01 23:52:32 -0800

    update readme

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <img src={process.env.PUBLIC_URL + "/profile.png"} alt="about" />
       </div>
       <div className="right">
-        <Heading text="Who am I?" style={{ padding: "3rem" }} />
+        <Heading text="Who am I?" style={{ padding: "0 3rem 3rem" }} />
         <p>
         I am a Vancouver-based Software Developer with a BSc in Computer Science. Versatile in software engineering, I specialize in creating seamless applications and excel in full-stack web development. Proficient in various languages and tech stacks, I craft robust solutions by leveraging critical thinking and innovative approaches. Beyond my professional work, I actively engage in data analysis, applying my expertise to gain insights and refine analytical skills.
         </p>
